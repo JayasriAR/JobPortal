a56bba0537dcaabf38da9fe6d1526fd6d7cc36f0
Jayasri A R <109610628+JayasriAR@users.noreply.github.com>
2024-08-28 19:34:46 +0530

    Update service.js

--- JobPortalSignUp/frontend/src/service/service.js
+++ JobPortalSignUp/frontend/src/service/service.js
@@ -6,7 +6,7 @@ export const signupService = async (formData) => {
    
 console.log("data recieved in service",formData);
 
-    const response = await apiRequest('http://localhost:5000/api/users/signup', 'POST', formData);
+    const response = await apiRequest('https://jobportalbckendservice.onrender.com/api/users/signup', 'POST', formData);
     return response; 
   } catch (error) {
     throw error; 
